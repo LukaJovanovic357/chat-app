9cabf768f3b4c01bff626026063c5dfdbca3cf6c
LukaJovanovic357 <jovanovicluka276@outlook.com>
2025-06-24 10:11:16 +0300

    add auth controller boilerplate

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const signup = (req, res) => {
+    res.send("signup route");
+}
+
+const login = (req, res) => {
+    res.send("login route");
+}
+
+const logout = (req, res) => {
+    res.send("logout route");
+}
+
+export { signup, login, logout };
