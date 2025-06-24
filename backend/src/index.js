981fee305e22da4e778b080d70fbda136349011a
LukaJovanovic357 <jovanovicluka276@outlook.com>
2025-06-24 14:21:26 +0300

    add sign up controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ import { connectDB } from "../lib/db.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
